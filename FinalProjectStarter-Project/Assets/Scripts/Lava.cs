0d0a3a1f99610e5c254a5387b94ed25a96ee3715
atikarnk <khun0015@algonquinlive.com>
2024-11-24 07:03:35 -0500

    lava

---
+++ FinalProjectStarter-Project/Assets/Scripts/Lava.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Lava : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Mario"))
+        {
+            // Get the Mario component from the GameObject
+            Mario mario = collision.gameObject.GetComponent<Mario>();
+
+            if (collision.contacts.Length > 0 && collision.contacts[0].normal.y <= -0.8f)
+            {
+                mario.HandleDamage(true);
+            }
+        }
+    }
+}
