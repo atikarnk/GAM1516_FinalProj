5f9a294274849a435afaecfe7131901e03792a04
Filipe Botelho <bote0024@algonquinlive.com>
2024-11-25 17:12:36 -0500

    BoomBoom walking and death done

--- FinalProjectStarter-Project/Assets/Scripts/BoomBoom.cs
+++ FinalProjectStarter-Project/Assets/Scripts/BoomBoom.cs
@@ -23,6 +23,7 @@ public enum eBoomBoomState : byte
     Dormant,
     Jump,
     NearDeath,
+    NearDeathStunned,
     Death,
     Pause,
     MAX=Pause
@@ -48,28 +49,34 @@ public class BoomBoom : Enemy
     public Sprite m_dormantSprite;
     public Sprite m_walkingSprite;
     public Sprite m_stunnedSprite;
+    public Sprite m_nearDeathStunned;
     public Sprite m_deathSprite;
 
 
     public EGoombaInitialDirection m_initialDirection = EGoombaInitialDirection.Random;
     private eBoomBoomState m_state = eBoomBoomState.Unknown;
+
     private float m_stunnedDuration = 0.0f;
     private Vector2 m_velocity = Vector2.zero;
-    private float m_directionChangeInterval = 0.0f; 
+    private float m_directionChangeInterval = 0.0f;
+    private bool m_isInvincible = true;
     // Start is called before the first frame update
-    void Start()
+    protected override void Start()
     {
+        base.Start();
         m_velocity.x = EnemyConstants.c_boomBoomSpeed;
         m_stunnedDuration = EnemyConstants.c_boomBoomStunnedDuration;
-        m_state = eBoomBoomState.Dormant;
+        SetState(eBoomBoomState.Dormant);
         m_lives = EnemyConstants.c_boomBoomLives;
         m_directionChangeInterval= EnemyConstants.c_boomBoomDirectionChangeInterval;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
+       
     }
     private void FixedUpdate()
     {
@@ -86,6 +93,36 @@ public class BoomBoom : Enemy
             location += m_velocity * Time.deltaTime * Game.Instance.LocalTimeScale;
             rigidbody.position = location;
         }
+        if (m_state == eBoomBoomState.Dormant)
+        {
+
+            m_stunnedDuration -= Time.deltaTime * Game.Instance.LocalTimeScale;
+
+            if (m_stunnedDuration <= 0.0f)
+            {
+                m_stunnedDuration = EnemyConstants.c_boomBoomStunnedDuration;
+                SetState(eBoomBoomState.Walking);
+            }
+        }
+        if (m_state == eBoomBoomState.Stunned|| m_state == eBoomBoomState.NearDeathStunned)
+        {
+            m_stunnedDuration -= Time.deltaTime * Game.Instance.LocalTimeScale;
+
+            if (m_stunnedDuration <= 0.0f)
+            {
+                m_stunnedDuration = EnemyConstants.c_boomBoomStunnedDuration;
+                SetState(eBoomBoomState.Dormant);
+            }
+        }
+        if (m_state == eBoomBoomState.Death)
+        {
+            m_stunnedDuration -= Time.deltaTime * Game.Instance.LocalTimeScale;
+
+            if (m_stunnedDuration <= 0.0f)
+            {
+                Destroy(gameObject);
+            }
+        }
     }
     private void SetState(eBoomBoomState state)
     {
@@ -94,32 +131,44 @@ public class BoomBoom : Enemy
             m_state = state;
             if (m_state == eBoomBoomState.Walking)
             {
+                m_isInvincible = false;
                 spriteRenderer.sprite = m_walkingSprite;
             }
             if (m_state == eBoomBoomState.Stunned)
             {
+                m_isInvincible = true;
                 spriteRenderer.sprite = m_stunnedSprite;
             }
             if (m_state == eBoomBoomState.Dormant)
             {
+                m_isInvincible = true;
                 spriteRenderer.sprite = m_dormantSprite;
             }
             if (m_state == eBoomBoomState.Jump)
             {
+                m_isInvincible = false;
                 spriteRenderer.sprite = m_walkingSprite;
             }
             if (m_state == eBoomBoomState.NearDeath)
             {
+                m_isInvincible = false;
                 m_velocity.x = EnemyConstants.c_boomBoomNearDeathSpeed;
                 spriteRenderer.sprite = m_walkingSprite;
             }
             if (m_state == eBoomBoomState.Death)
             {
+                m_isInvincible = true;
                 spriteRenderer.sprite = m_deathSprite;
             }
             if (m_state == eBoomBoomState.Pause)
             {
+                m_isInvincible = false;
                 spriteRenderer.sprite = m_walkingSprite;
+            }
+            if (m_state == eBoomBoomState.NearDeathStunned)
+            {
+                m_isInvincible = true;
+                spriteRenderer.sprite = m_nearDeathStunned;
             }
         }
         
@@ -147,8 +196,127 @@ public class BoomBoom : Enemy
 
     private void RandomizeDirection()
     {
+        //TODO check near death
         int index = UnityEngine.Random.Range(0, 10) % 2;
         float[] speeds = { EnemyConstants.c_boomBoomSpeed, -EnemyConstants.c_boomBoomSpeed };
         m_velocity.x = speeds[index];
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Is the Goomba colliding with the Mario GameObject?
+        if (collision.gameObject.CompareTag("Mario"))
+        {
+            // Get the Mario component from the GameObject
+            Mario mario = collision.gameObject.GetComponent<Mario>();
+
+            // Check if there's a contact object, in the contacts array
+            if (collision.contacts.Length > 0)
+            {
+                // Get the normal from the first contact object
+                Vector2 normal = collision.contacts[0].normal;
+
+                // Ensure the Goomba's state is walking
+                if (m_state == eBoomBoomState.Walking)
+                {
+                    if (m_state == eBoomBoomState.Dormant)
+                    {
+                        mario.HandleDamage();
+                    }
+                    if (normal.x <= -0.8f || normal.x >= 0.8f)
+                    {
+                        // Goomba collided with Mario on the side
+                        mario.HandleDamage();
+                    }
+                    else if (normal.y >= 0.7f)
+                    {
+                        // Goomba landed on Mario
+                        mario.HandleDamage();
+                    }
+                    else if (normal.y <= -0.7f)
+                    {
+                        
+                        if (!m_isInvincible) 
+                        {
+                            m_lives--;
+                            // Mario landed on the Goomba, make Mario bounce off the enemy
+                            MarioMovement marioMovement = collision.gameObject.GetComponent<MarioMovement>();
+                            marioMovement.ApplyBounce();
+                            if (m_lives > 1)
+                            {
+
+                                SetState(eBoomBoomState.Stunned);
+                            }
+                            else if (m_lives == 1)
+                            {
+
+                                SetState(eBoomBoomState.NearDeathStunned);
+                            }
+                            else
+                            {
+                                SetState(eBoomBoomState.Death);
+                            }
+                        }
+                        
+                    }
+                }
+            }
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        bool flipMovementDirection = false;
+
+        if (other.gameObject.CompareTag("World"))
+        {
+            // The Goomba collided with the World
+            flipMovementDirection = true;
+        }
+        else if (other.gameObject.CompareTag("Enemy"))
+        {
+            // The Goomba collided with another Enemy
+            EEnemyType enemyType = other.gameObject.GetComponent<Enemy>().EnemyType;
+
+            flipMovementDirection = true;
+            
+        }
+        else if (other.gameObject.CompareTag("Pickup"))
+        {
+            flipMovementDirection = true;
+        }
+
+        // DO we need to flip the Goomba's direction
+        if (flipMovementDirection)
+        {
+            // Get the Goomba's overlapping collider
+            ContactFilter2D filter = new ContactFilter2D().NoFilter();
+            List<Collider2D> results = new List<Collider2D>();
+            other.OverlapCollider(filter, results);
+
+            // Set the Goomba's velocity based on which collider was triggered 
+            if (results.Contains(frontTrigger))
+            {
+                if (m_lives <= 1)
+                {
+                    m_velocity.x = -EnemyConstants.c_boomBoomNearDeathSpeed;
+                }
+                else
+                {
+                    m_velocity.x = -EnemyConstants.c_boomBoomSpeed;
+                }
+
+            }
+            else if (results.Contains(backTrigger))
+            {
+                if (m_lives <= 1)
+                {
+                    m_velocity.x = EnemyConstants.c_boomBoomNearDeathSpeed;
+                }
+                else
+                {
+                    m_velocity.x = EnemyConstants.c_boomBoomSpeed;
+                }
+            }
+        }
     }
 }
